2eaa49196f565baf65f1bc8afe94e8db671edc73
Exitroz <ezeobih@gmail.com>
2023-09-28 16:47:53 +0100

    static

--- psychiatric_website/settings.py
+++ psychiatric_website/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 if DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
