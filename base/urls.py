1c829a05661bf46651b1a4a362397ff5470e8114
Exitroz <ezeobih@gmail.com>
2023-09-29 12:02:16 +0100

    design update

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('doctors', views.doctors, name="doctors"),
+    path('gallery', views.gallery, name="gallery"),
+    path('services', views.services, name="services"),
+    path('faq', views.faq, name="faq"),
+    path('appointment', views.appointment, name="appointment"),
+    path('contact', views.contact, name="contact"),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def doctors(request):
+    return render(request, "doctors.html")
+
+def services(request):
+    return render(request, "services.html")
+
+def gallery(request):
+    return render(request, "gallery.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def appointment(request):
+    return render(request, "appointment.html")
+
+def faq(request):
+    return render(request, "faq.html")
+
